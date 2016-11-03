8274279fd2b725a6b3ab7e87fededfca411925f3
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-11-03 11:31:14 +0100

    added A3 functionality

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
-app.get(/.*/, function(req, res){
+
+app.get(/\/public\/.*/, function(req, res) {
+    const errorFile = path.join(__dirname + '/error.html');
+    const reqPath = path.join(__dirname + '/static/' + req.originalUrl.substring(8))
+    const exists = fs.existsSync(reqPath);
+
+    if(exists){
+        res.sendFile(path.join(reqPath));
+    }else{
+        res.sendFile(errorFile);
+    }
+});
+
+app.get(/.*/, function(req, res) {
     res.sendFile(path.join(__dirname + '/helloworld.html'));
-})
+});
 
-app.listen(3000, () => console.log("hallo!") );
-
-//app.use(express.static('static'); Zusatz: Nur bei /public ?
+app.listen(3000, () => console.log("hallo!"));
