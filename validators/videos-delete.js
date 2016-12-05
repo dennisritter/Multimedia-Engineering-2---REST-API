65fa3784fbcae786898f038d5d9d79204586b15b
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-12-05 14:41:13 +0100

    changed delete validator to handle testdata as expected

--- validators/videos-delete.js
+++ validators/videos-delete.js
@@ -1,11 +1,9 @@
 var store = require('../blackbox/store');
-const internalKeys = {id: 'number', timestamp: 'number'};
+const internalKeys = {id: 'number', timestamp: 'string'};
 
-const validateVideoDEL = function(data) {
+const validateVideoDEL = function(id) {
     "use strict";
-
-    const video = data.id;
-
+    var video = id;
     // Check for missing id
     if (!video) {
         const err = new Error(`Please send a valid id.`);
@@ -13,28 +11,25 @@ const validateVideoDEL = function(data) {
         throw err;
     }
 
+    //if id sent as a string, parse it so type changes to number
+    if(typeof video === 'string'){
+        video = parseInt(video, 10);
+    }
     // Check for valid id type
     if (typeof video !== internalKeys.id) {
-        const err = new Error(`Property of id must be of type ${internalKeys.id}`);
+        const err = new Error(`Property of id must be of type ${internalKeys.id} or a parsable string`);
         err.status = 404;
         throw err;
     }
 
-    // Remove invalid properties
-    for (let key in data) {
-        if (key !== "id") {
-            delete data[key];
-        }
-    }
-
     //Check for existing video-id in store
-    if (store.select('videos', video) == undefined) {
+    if (!store.select('videos', video)) {
         const err = new Error(`An element with ID ${video} does not exist.`);
         err.status = 404;
         throw err;
     }
 
-    return data;
+    return video;
 };
 
 module.exports = validateVideoDEL;
