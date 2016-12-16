56c69d765e3957c50ed289e9f4d1798cc4b1f151
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-12-16 19:46:13 +0100

    implemented videoSchema and model export

--- models/video.js
+++ models/video.js
@@ -4,6 +4,27 @@
  * The mongoose model for a Video.
  */
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const VideoSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: false, default: ""},
+    src: {type: String, required: true},
+    length: {type: Number, required: true, min: 0},
+    playcount: {type: Number, required: false, min: 0, default: 0},
+    ranking: {type: Number, required: false, min: 0, default: 0}
+}, {
+   timestamps: {createdAt: 'timestamp'}
+});
 
+module.export = mongoose.model('Video', VideoSchema);
 
+// _id (String, von Außen nicht setzbar, automatisch bei POST)
+//  title (String, required)
+//  description (String, optional, default '' [leerer String])
+//  src (String, required)
+//  length (Number; nicht negative Zahl für Sekundenangabe, required)
+//  timestamp (String, nicht von Außen setzbar, automatisch bei POST)
+//  playcount (Number; nicht negative Zahl, optional, default 0)
+//  ranking (Number; nicht negative Zahl, optional, default 0)
